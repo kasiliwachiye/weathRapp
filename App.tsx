382e5cd28369abdb5323fe7eb8ba94852240f9bc
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-08-05 14:16:12 +0300

    feat: add CardMain component

--- App.tsx
+++ App.tsx
@@ -1,6 +1,25 @@
-import React from "react";
-import Home from "./src/pages/home";
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+
+//screen
+import Home from "./src/pages/Home";
 
 export default function App() {
-  return <Home />
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return <Home />;
+  }
 }
